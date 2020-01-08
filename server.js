8e6165b94e030fa911ed708186f494c80d29870a
chernandezUV <chernandez@unitedvirtualities.com>
2020-01-08 15:36:43 -0700

    Server start

---
+++ server.js
@@ -0,0 +1,7 @@
+const  express  =  require ('express');
+const aplicación  = express ();  
+
+
+aplicación . listen ( 8000 , function () {
+    console . log ( "El servidor se está ejecutando " )
+   })
